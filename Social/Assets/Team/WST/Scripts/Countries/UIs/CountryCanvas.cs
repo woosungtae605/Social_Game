00de4dbf677b1a9adf239c908511968ef00995c9
woosungtae <a01076755795@gmail.com>
2026-07-16 12:16:46 +0900

    feat : countryDetail

--- Social/Assets/Team/WST/Scripts/Countries/UIs/CountryCanvas.cs
+++ Social/Assets/Team/WST/Scripts/Countries/UIs/CountryCanvas.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Team.WST.Scripts.Countries.UIs.CountryDetailUIs;
+﻿using Team.WST.Scripts.Countries.UIs.CountryDetailUIs;
 using Team.WST.Scripts.Countries.UIs.CountryInformationUIs;
 using UnityEngine;
 
@@ -9,10 +8,34 @@ namespace Team.WST.Scripts.Countries.UIs
     {
         [SerializeField] private CountryDetailCanvas countryDetailCanvas;
         [SerializeField] private CountryInformationCanvas countryInformationCanvas;
+        [SerializeField] private GameObject countryDetailObject;
+        [SerializeField] private GameObject countryInformationObject;
 
         private void Awake()
         {
+            countryDetailCanvas.Init();
             countryInformationCanvas.Init();
+
+            countryDetailCanvas.OnExitBtnClick += HandleExitBtnClick;
+            countryInformationCanvas.OnMoreViewBtnClick += HandleMoreViewBtnClick;
+        }
+
+        private void OnDestroy()
+        {
+            countryInformationCanvas.OnMoreViewBtnClick -= HandleMoreViewBtnClick;
+            countryDetailCanvas.OnExitBtnClick -= HandleExitBtnClick;
+        }
+
+        private void HandleMoreViewBtnClick(ICultureShowUI obj)
+        {
+            countryDetailObject.gameObject.SetActive(true);
+            countryInformationObject.gameObject.SetActive(false);
+        }
+        
+        private void HandleExitBtnClick()
+        {
+            countryDetailObject.gameObject.SetActive(false);
+            countryInformationObject.gameObject.SetActive(true);
         }
     }
 }
--- Social/Assets/Team/WST/Scripts/Countries/UIs/CountryDetailUIs/CountryDetailCanvas.cs
+++ Social/Assets/Team/WST/Scripts/Countries/UIs/CountryDetailUIs/CountryDetailCanvas.cs
@@ -1,9 +1,34 @@
-﻿using UnityEngine;
+﻿using System;
+using Team.WST.Scripts.Countries.UIs.CountryInformationUIs;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace Team.WST.Scripts.Countries.UIs.CountryDetailUIs
 {
     public class CountryDetailCanvas : MonoBehaviour
     {
-        
+        [Header("UIs")]
+        [SerializeField] private Button exitBtn;
+
+        public event Action OnExitBtnClick; 
+        public void Init()
+        {
+            exitBtn.onClick.AddListener(HandleExitBtnClick);
+        }
+
+        private void OnDestroy()
+        {
+            exitBtn.onClick.RemoveListener(HandleExitBtnClick);
+        }
+
+        private void HandleExitBtnClick()
+        {
+            OnExitBtnClick?.Invoke();
+        }
+
+        public void Show(ICultureShowUI iCultureShowUI)
+        {
+            
+        }
     }
 }
